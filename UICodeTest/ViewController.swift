9dd2cfa37daca6b7822cd1d068b1c94263079a34
c0407h <67133244+c0407h@users.noreply.github.com>
2023-11-20 20:00:42 +0900

    Initial Commit

---
+++ UICodeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICodeTest
+//
+//  Created by Chung Wussup on 11/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
