d5a6c5590c6fb8c1e145280e2af53cf7bc74f071
c0407h <67133244+c0407h@users.noreply.github.com>
2023-11-20 20:24:43 +0900

    Code로 UI짜기 연습 1

--- UICodeTest/ViewController.swift
+++ UICodeTest/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Code UI Test
+    
+    let emailTextFieldView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeUI()
+    }
+    
+    
+    func makeUI() {
+        emailTextFieldView.backgroundColor = .gray
+        
+        view.addSubview(emailTextFieldView)
+        
+        
+        //⭐️ .translatesAutoresizingMaskIntoConstraints -> 자동으로 잡아주는 오토레이아웃을 off 하는 것
+        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //.isActive = true로 해줘야 적용됨
+        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        
+        //height, width는 기준이 없기 때문에 constraint(equalToConstant:) 사용
+        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        emailTextFieldView.layer.cornerRadius = 20
+        emailTextFieldView.layer.masksToBounds = true
     }
 
 
